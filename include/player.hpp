9a61a08ec63f0e11bb15558dc5b6d66660ae0541
cthornberry <cthornberry@ratwarrior>
2025-11-14 15:38:05 -0800

    player base stats

--- include/player.hpp
+++ include/player.hpp
@@ -6,6 +6,9 @@ class Player {
 private:
     std::string name;
     std::vector<std::string> inventory;
+    int strength;
+    int intelligence;
+    int charisma;
 public:
     Player(const std::string& playerName);
     std::string getName() const;
